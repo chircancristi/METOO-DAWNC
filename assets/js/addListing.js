ab19186ec24947871468ef575462696c0c36f71b
chircancristi <cristianchircan1@gmail.com>
2018-10-31 12:37:06 +0200

    Cod fixes

--- assets/js/addListing.js
+++ assets/js/addListing.js
@@ -1,36 +1,36 @@
-var submitButton = document.querySelector('#submit');
-var order=1;
-var curentItemId ;
-var nextItemId;
+const submitButton = document.querySelector('#submit');
+let order=1;
+let curentItemId ;
+let nextItemId;
 
 function changeItem()
 {
-    if (order==1) {
+    if (order===1) {
         curentItemId ='first';
         nextItemId='second';
     }
-    if (order==2) {
+    if (order===2) {
         curentItemId ="second";
         nextItemId="third";
     }
-    if (order==3) {
+    if (order===3) {
         curentItemId ="third";
         nextItemId="fourth";
     }
-    if (order==4) {
+    if (order===4) {
         curentItemId ="fourth";
         nextItemId="fifth";
     }
-    if (order==5) {
+    if (order===5) {
         curentItemId ="fifth";
     }
-
-    nextLine=document.getElementById(nextItemId);
-    document.querySelector("#"+nextItemId+" span").style.backgroundColor = "hsla(204, 70%, 53%, 0.9)";
+    let nextItemSpanSelector="#"+nextItemId+" span";
+    let nextLine=document.getElementById(nextItemId);
+    document.querySelector(nextItemSpanSelector).style.backgroundColor = "hsla(204, 70%, 53%, 0.9)";
     nextLine.style.color="hsla(204, 70%, 53%, 0.9)";
 
-    nextSection=document.getElementById(nextItemId+"Question");
-    currentSection=document.getElementById(curentItemId+"Question");
+    let nextSection=document.getElementById(nextItemId+"Question");
+    let currentSection=document.getElementById(curentItemId+"Question");
 
     currentSection.style.transform="translateY(-80em)";
     currentSection.style.opacity=0;
