8c385381c997561163b149dc33006efe34ae3661
razsbg <sbingu.razvan@gmail.com>
2018-11-02 06:26:31 +0200

    mobile navigation + responsive using media query mixin

---
+++ assets/js/mobile-nav.js
@@ -0,0 +1,10 @@
+const navTrigger = document.getElementById('js-nav-trigger'); 
+const navClose = document.getElementById('js-nav-close');
+const navWrapper = document.getElementById('js-nav-wrapper');
+
+navTrigger.addEventListener('click', () => {
+  navWrapper.classList.toggle('is-visible');
+});
+navClose.addEventListener('click', () => {
+  navWrapper.classList.toggle('is-visible');
+});
